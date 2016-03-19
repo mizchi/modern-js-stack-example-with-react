6f7bbde53deb1c5b5f75775cd21e0d4921fc65e1
Koutarou Chikuba <miz404@gmail.com>
2016-03-20 02:41:30 +0900

    Fix container

--- src/framework/component.js
+++ src/framework/component.js
@@ -1,3 +1,12 @@
-import {Component} from "react";
+/* @flow */
+import {Component, PropTypes} from "react";
 export default class ComponentBase extends Component {
+  dispatch(...args: any[]): boolean {
+    return this.context.dispatch(...args);
+  }
 }
+
+ComponentBase.contextTypes = {
+  dispatch: PropTypes.any,
+  rootProps: PropTypes.any
+};
--- src/framework/container.js
+++ src/framework/container.js
@@ -1,3 +1,28 @@
-import {Component} from "react";
-export default class ContainerBase extends Component {
+/* @flow */
+import {Component, PropTypes} from "react";
+import {EventEmitter} from "events";
+
+export default class Container extends Component {
+  emitter: EventEmitter;
+
+  constructor(...args: any[]) {
+    super(...args);
+    this.emitter = new EventEmitter;
+  }
+
+  componentWillUnmount() {
+    this.emitter.removeAllListeners();
+  }
+
+  getChildContext(): any {
+    return {
+      dispatch: this.emitter.emit.bind(this.emitter),
+      rootProps: this.props
+    };
+  }
 }
+
+Container.childContextTypes = {
+  dispatch: PropTypes.any,
+  rootProps: PropTypes.any
+};
